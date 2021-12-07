29611bfadb825d0122f2e6c58a07390c624a982b
Bastiaan Tiobook <deknudtb@gmail.com>
2021-12-07 23:30:03 +0100

    show timer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Manager, ProduceAction } from 'queueDirector';
 
+  import { onMount } from 'svelte';
+
   const dir = new Manager();
   dir.createProducer();
 
@@ -9,17 +11,30 @@
   $: if(!isNaN(selIdx)) selected = dir.producers[selIdx];
 
   function createNewInSelected() {
-    selected.enque((newP) => new ProduceAction(1, newP));
+    selected.enque((newP) => new ProduceAction(10, newP));
     dir.producers = dir.producers;
   }
   function cancelAction(action){
     selected.cancelAction(action);
     selected.actionQueue = selected.actionQueue;
   }
+
+  onMount(()=>{
+    let prev = performance.now();
+    function loop(){
+      let now = performance.now();
+      const dt = (now-prev)/1000;
+      dir.evaluate(dt);
+      prev = now;
+      dir=dir;
+      requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+  });
 </script>
 
 {#each dir.producers as p, i}
-<button class:paused={p.paused} on:click={(e) => (selIdx = i)}>{p.id}</button>
+<button class:paused={p.paused} on:click={(e) => (selIdx = i)}>{p.id} {p.actionQueue.length||''}</button>
 {/each}
 
 <br>
@@ -30,7 +45,7 @@
   <button on:click={createNewInSelected}>create</button>
   <br>
   {#each selected.actionQueue as action}
-  <button on:click={e=>cancelAction(action)}>{action.id}</button>
+  <button on:click={e=>cancelAction(action)}>{action.id} {action.timeLeft}</button>
   {/each}
 {/if}
 
